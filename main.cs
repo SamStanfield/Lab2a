b56fc4ca98e3c6814516a1e6aaf8acba0911e530
20sstanfield <20sstanfield@gmail.com>
2021-02-04 02:04:34 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Enter the name of item.");
+    string name = Console.ReadLine();
+    Console.WriteLine ("Enter the quantity.");
+    int quantity = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine ("Enter the price.");
+    double price = Convert.ToDouble(Console.ReadLine());
+
+    double total = quantity * price;
+
+    Console.WriteLine(" the total price of " + name + " is $" + total);
+  }
+}
